987114e789dbe9664ec5bc66de9fb0b957625634
jawid fakhari <jawid_fakhari@live.it>
2024-07-18 12:18:04 +0200

    earth map texture added, starfield added, directional light added to simulate sun light

--- index.js
+++ index.js
@@ -1,60 +1,66 @@
+// https://planetpixelemporium.com/earth.html
 import * as THREE from "three";
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import getStarfield from "./src/getStarfield.js";
 
 // Create Scene
-
 const scene = new THREE.Scene();
-
 // Create Camera
-
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
-camera.position.z = 5;
-
+camera.position.z = 3;
 // Create Renderer
-
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add OrbitControls
+//////////////////////////
+// Create Earth Group Object
+const earthGroup = new THREE.Group();
+// calculate Earth's tilt angle
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+// Add Earthgrpup to scene
+scene.add(earthGroup);
 
+const stars = getStarfield({ numStars: 2000 });
+scene.add(stars);
+
+// Add OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Create loader object
+const loader = new THREE.TextureLoader();
+
 // Create Geometry
-
 const geometry = new THREE.IcosahedronGeometry(1, 12);
-
 // Create Material
-
 const material = new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
-  flatShading: true,
+  map: loader.load("/assets/earth_map.jpg"),
+  // flatShading: true,
 });
 
 // Create Mesh
-
 const earthMesh = new THREE.Mesh(geometry, material);
-earthMesh.rotation.x = 5;
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
 
 // Lights
-
-const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x000000, 1);
-scene.add(hemisphereLight);
-
+// const hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x000000, 1);
+// scene.add(hemisphereLight);
+// sun light
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2, 0.1, 1);
+scene.add(sunLight);
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
 
-  earthMesh.rotation.z += 0.001;
-  // cube.rotation.y += 0.001;
+  earthMesh.rotation.y += 0.002;
 
+  // cube.rotation.y += 0.001;
   renderer.render(scene, camera);
 }
 
